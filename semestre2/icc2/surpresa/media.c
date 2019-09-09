7adae72244b2e703913456084bcb0e5973cd0f92
brenocq <breno1423@gmail.com>
2019-09-08 21:03:41 -0300

    Feat: Mean with chained list

--- semestre2/icc2/surpresa/media.c
+++ semestre2/icc2/surpresa/media.c
@@ -6,13 +6,15 @@ typedef struct _elemento{
   struct _elemento *next;
 }elemento;
 
+float mediaIterativa(elemento *ini);
+float mediaRecursiva(elemento *ini, int qtd);
+
 int main(int argc, char const *argv[]) {
   int qtd, i;
   float val;
 
   scanf("%d", &qtd);
   scanf("%f", &val);
-
   elemento* first = (elemento*)malloc(sizeof(elemento));
   first->value = val;
 
@@ -29,10 +31,30 @@ int main(int argc, char const *argv[]) {
     last = ele;
   }
 
-  elemento *print = *ini;
-  while(print!=NULL) {
-    printf("%f\n", print->value);
-    print=print->next;
-  }
+  float media1 = mediaIterativa(*ini);
+  float media2 = mediaRecursiva(*ini, qtd);
+
+  printf("%.2f\n", media1);
+  printf("%.2f\n", media2);
+
   return 0;
+}
+
+float mediaIterativa(elemento *ini){
+    float soma = 0;
+    int qtd = 0;
+    while(ini!=NULL) {
+        qtd++;
+        soma += ini->value;
+        ini=ini->next;
+    }
+    return soma/qtd;
+}
+
+float mediaRecursiva(elemento *ini, int qtd){
+    if(ini==NULL){
+        return 0;
+    }else{
+        return (ini->value + mediaRecursiva(ini->next, qtd-1)*(qtd-1)) / qtd;
+    }
 }
