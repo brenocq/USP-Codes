1457c9c82ad9a33e7c0d8fd425a610cc1ea02e66
madukm <48024589+madukm@users.noreply.github.com>
2019-09-15 14:40:38 -0300

    Update relatorio.h

--- semestre2/algI/relatorioAlunos/relatorio.h
+++ semestre2/algI/relatorioAlunos/relatorio.h
@@ -5,10 +5,13 @@
 #define bool char 
 #define true 1
 #define false 0
+#define TAM_MAX 1000
 
 typedef struct _aluno Aluno;
 typedef struct _registros Registros;
 
+bool registro_cheio(Registros *r);
+bool registro_vazio(Registros *r);
 Registros* registro_criar();// Criar um registro vazio
 Aluno* aluno_criar(int id, float horasDeEstudo, float nota1, float nota2);// Cria elemento de aluno
 bool registro_deletar(Registros *r);// Deleta o registro
