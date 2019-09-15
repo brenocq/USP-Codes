cb4e7049d89172a349992c267ef10d055674089b
madukm <48024589+madukm@users.noreply.github.com>
2019-09-15 14:47:47 -0300

    Update main.c

--- semestre2/algI/relatorioAlunos/main.c
+++ semestre2/algI/relatorioAlunos/main.c
@@ -4,6 +4,41 @@
 
 
 int main(int argc, char const *argv[]) {
+  while(1){
+    int funcao;
+    printf("MENU\n");
+    printf("Escolha uma das funções abaixo colocando o número correspondente:\n);
+    printf("Função 1: Inserir Registro\n");
+    printf("Função 2: Remover Registro\n");
+    printf("Função 3: Imprimir Registros\n");
+    printf("Função 4: Relatório de Aprovação\n");
+    printf("Função 5: Horas de Estudo\n");
+    printf("Função 6: Sair\n");
+    switch(scanf("%d", &funcao)){
+      case 1: 
+      
+          break;
+      case 2:
+      
+          break;
+      
+      case 3:
+      
+          break;
+      
+      case 4:
+      
+          break;
+      
+      case 5:
+      
+          break;
+      
+      case 6:
+      
+          exit(1);
+    }
+  }
   Registros *registro = registro_criar();
   Aluno* aluno1 = aluno_criar(0,5,10,9);
   Aluno* aluno2 = aluno_criar(4,5,10,9);
