2c4b5947756c023ee85c473951d97e09a6343544
Breno Cunha Queiroz <breno1423@gmail.com>
2019-09-20 13:38:09 -0300

    Feat: Sorting with c

---
+++ semestre2/icc2/sorting.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+#define SIZE 10
+
+void shellSort(int *vec, int subVecSize);
+
+int main(int argc, char const *argv[]) {
+  int *vec = (int*)malloc(sizeof(int)*SIZE);
+
+  return 0;
+}
+
+void ordenacaoPorInsercao(int *vec, int subVecSize){
+  //if()
+}
