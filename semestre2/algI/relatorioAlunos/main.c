ceee5fbdec01a3a9a5228b681b08b30a86e1d52c
Breno Cunha Queiroz <breno1423@gmail.com>
2019-09-20 13:40:51 -0300

    Style: Comments deleted

--- semestre2/algI/relatorioAlunos/main.c
+++ semestre2/algI/relatorioAlunos/main.c
@@ -85,17 +85,5 @@ int main(int argc, char const *argv[]) {
             exit(1);
     }
   }
-
-  /*Aluno* aluno2 = aluno_criar(4,5,10,9);
-  Aluno* aluno3 = aluno_criar(1,5,10,9);
-
-  registro_inserir(registro, aluno3);
-  registro_remover(registro, 1);
-  registro_remover(registro, 0);
-  registro_inserir(registro, aluno2);
-
-  registro_imprimir(registro);*/
-
-  //registro_deletar(registro);
   return 0;
 }
