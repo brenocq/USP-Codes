2c21223e05af6e24e168a61c0084021ac0ecd5b0
madukm <kawakamimadu@gmail.com>
2019-10-20 10:44:43 -0300

    Feat: Pilha

--- semestre2/algI/Projeto2/pilha.c
+++ semestre2/algI/Projeto2/pilha.c
@@ -2,36 +2,79 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-Pilha* pilha_criar(){
+#define MAX 50
 
+struct _item{
+  Carro *carro;
+  struct _item *prev;
+};
+
+struct _pilha{
+  Item *topo;
+  int tamanho;
+};
+
+Pilha* pilha_criar(){
+  Pilha *p = (Pilha *)malloc(sizeof(Pilha));
+  if(p != NULL){
+    p->topo = NULL;
+    p->tamanho = 0;
+  }
+  return p;
 }
 
 Item* item_criar(Carro *c){
-
+  Item *i = (Item *)malloc(sizeof(Item));
+  if(i != NULL){
+    i->carro = c;
+    i->prev = NULL;
+  }
+  return i;
 }
 
 void pilha_deletar(Pilha **p){
-
+  Item *i = (*p)->topo;
+  while(n != NULL){
+    Item *prox = i->prev;
+    free(i);
+    n = next;
+  }
+  free(*p);
+  *p = NULL;
 }
 
-Carro* pilha_inserir(Pilha *p){
-
+bool pilha_inserir(Pilha *p, Carro *c){
+  if(pilha_cheia) return FALSE;
+  Item *i = item_criar(c);
+  if(i != NULL){
+    i->prev = p->topo;
+    p->tamanho++;
+    p->topo = i;
+    return TRUE;
+  }
+  return FALSE;
 }
 
 Carro* pilha_remover(Pilha *p){
-
+  if(fila_vazia) return NULL;
+  Item *i = p->topo;
+  p->topo = p->topo->prev;
+  i->prev = NULL;
+  p->tamanho--;
+  return i->carro;
 }
 
 bool pilha_vazia(Pilha *p){
-
+  return (p->tamanho == 0);
 }
 
 bool pilha_cheia(Pilha *p){
-
+  return (p->tamanho == MAX);
 }
 
 Carro* pilha_topo(Pilha *p){
-
+  if(pilha_vazia) return NULL;
+  return p->topo->carro;
 }
 
 void pilha_checkout(Pilha *p, int horaSaida){
